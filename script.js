b19531eb123f35267cbad74e154c30c50d847a11
Umair-Jamaat-Ali <umairansari758@gmail.com>
2023-03-29 22:09:38 +0500

    practice1

--- script.js
+++ script.js
@@ -1,144 +1,144 @@
 
-// // 1-writ a js program to find maximum between two numbers.
+// 1-writ a js program to find maximum between two numbers.
 
-// var num1 =+ prompt("Enter first number");
-
-
-// var num2 =+ prompt("Enter second number");
-
-// if(num1>num2){
-//     console.log("First number is greater than r");
-// }
-// else{
-//     console.log("Second number is greater than ");
-// }
-
-// // 2- writ a js program to find maximum between three numbers.
-
-// var number1 =+ prompt("enter 1st number");
+var num1 =+ prompt("Enter first number");
 
 
-// var number2 =+ prompt("enter 2nd number");
+var num2 =+ prompt("Enter second number");
+
+if(num1>num2){
+    console.log("First number is greater than r");
+}
+else{
+    console.log("Second number is greater than ");
+}
+
+// 2- writ a js program to find maximum between three numbers.
+
+var number1 =+ prompt("enter 1st number");
 
 
-// var number3 =+ prompt("enter 3rd number");
-
-// if(number1>number2 && number1>number3){
-//     console.log("number 1st is greater than others")
-// }
+var number2 =+ prompt("enter 2nd number");
 
 
-// else if(number2>number1 && number2>number3){
-//     console.log("number 2nd is greater than others")
-// }
+var number3 =+ prompt("enter 3rd number");
+
+if(number1>number2 && number1>number3){
+    console.log("number 1st is greater than others")
+}
 
 
-//  else if(number3>number2 && number3>number1){
-//     console.log("number 3rd is greater than others")
-// }
+else if(number2>number1 && number2>number3){
+    console.log("number 2nd is greater than others")
+}
 
 
-
-// // 3- write a program to check whether a number is negative, positive or zero.
-
-
-// var x =+ prompt ("Enter a number");
-
-// if(x>0){
-//     console.log("Number is positive");
-// }
-
-
-// else if(x==0){
-//     console.log("Number is zero");
-// }
-
-
-// else if(x<0){
-//     console.log("Number is negative");
-// }
-
-
-// // 5- write a program to check the number is even or odd?
-
-// var y =+ prompt("Enter a number");
-
-// if(y%2==0){
-//     console.log("number is even ")
-// }
-
-// else{
-//     console.log("number is odd")
-// }
-
-// // 4- Write a js program to check whether a number is divisible by 5 and 11 or not
-
-
-// var x1 =+ prompt("enter a number");
-
-// if(x1%5==0 && x1%11==0){
-//     console.log("number is divided on 5 and 11");
-// }
-
-// else{
-//     console.log("number is not divided on 5 and 11");
-// }
-
-// // 6-Write a js program to check whether a year is leap year or not
-
-// var x2 =+ prompt("enter februry month days")
-
-// if(x2 == 29){
-//     console.log("This year is leap year")
-// }
-
-// else{
-//     console.log("This year is not leap year")
-// }
+ else if(number3>number2 && number3>number1){
+    console.log("number 3rd is greater than others")
+}
 
 
 
-// // 7. Write a js program to check whether a character is alphabet or not.
-
-// var alpha = prompt("write any things");
-
-// if(alpha >= "a" && alpha <= "z"){
-//     console.log("Characters are alphabet")
-// }
-
-// else{
-//     console.log("Characters are not alphabet")
-// }
+// 3- write a program to check whether a number is negative, positive or zero.
 
 
-// // 9.Write a js program to input any character and check whether it is alphabet, digit or special character.
+var x =+ prompt ("Enter a number");
 
-// var char =+ prompt("enter any things");
-
-// if(char >= "a" && char <= "z"){
-//     console.log("Characters are alphabet")
-// }
-
-// else if(char >= 0 && char <= 9){
-//     console.log("Characters are number")
-// }
-
-// else{
-//     console.log("Characters are special symbols")
-// }
+if(x>0){
+    console.log("Number is positive");
+}
 
 
-//8. Write a js program to input any alphabet and check whether it is vowel or consonant
+else if(x==0){
+    console.log("Number is zero");
+}
 
-// var vowelChar = prompt("enter any alphabets");
 
-// if(vowelChar === "a" || vowelChar === "e" || vowelChar === "i" || vowelChar === "o" || vowelChar === "u"){
-//     console.log("Characters are vowel alphabet")
-// }
+else if(x<0){
+    console.log("Number is negative");
+}
 
-// else{
-//     console.log("Characters are consonant alphabet")
-// }
+
+// 5- write a program to check the number is even or odd?
+
+var y =+ prompt("Enter a number");
+
+if(y%2==0){
+    console.log("number is even ")
+}
+
+else{
+    console.log("number is odd")
+}
+
+// 4- Write a js program to check whether a number is divisible by 5 and 11 or not
+
+
+var x1 =+ prompt("enter a number");
+
+if(x1%5==0 && x1%11==0){
+    console.log("number is divided on 5 and 11");
+}
+
+else{
+    console.log("number is not divided on 5 and 11");
+}
+
+// 6-Write a js program to check whether a year is leap year or not
+
+var x2 =+ prompt("enter februry month days")
+
+if(x2 == 29){
+    console.log("This year is leap year")
+}
+
+else{
+    console.log("This year is not leap year")
+}
+
+
+
+// 7. Write a js program to check whether a character is alphabet or not.
+
+var alpha = prompt("write any things");
+
+if(alpha >= "a" && alpha <= "z"){
+    console.log("Characters are alphabet")
+}
+
+else{
+    console.log("Characters are not alphabet")
+}
+
+
+// 8.Write a js program to input any character and check whether it is alphabet, digit or special character.
+
+var char =+ prompt("enter any things");
+
+if(char >= "a" && char <= "z"){
+    console.log("Characters are alphabet")
+}
+
+else if(char >= 0 && char <= 9){
+    console.log("Characters are number")
+}
+
+else{
+    console.log("Characters are special symbols")
+}
+
+
+//9. Write a js program to input any alphabet and check whether it is vowel or consonant
+
+var vowelChar = prompt("enter any alphabets");
+
+if(vowelChar === "a" || vowelChar === "e" || vowelChar === "i" || vowelChar === "o" || vowelChar === "u"){
+    console.log("Characters are vowel alphabet")
+}
+
+else{
+    console.log("Characters are consonant alphabet")
+}
 
 
 
